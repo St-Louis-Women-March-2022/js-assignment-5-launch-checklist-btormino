ef8155886fac1a6c6603e483fe3514d27706c023
Bethany <morphforlife@gmail.com>
2022-07-12 23:11:39 -0500

    finished faultyItems list

--- scriptHelper.js
+++ scriptHelper.js
@@ -35,7 +35,7 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
         let copilotStatus = document.getElementById('copilotStatus');
         let fuelStatus = document.getElementById('fuelStatus');
         let cargoStatus = document.getElementById('cargoStatus');
-
+   
    if (validateInput(pilot) === "Empty" || validateInput(copilot) === 'Empty' || validateInput(fuelLevel) === 'Empty' || validateInput(cargoLevel) === 'Empty' ){
       alert ('All fields are required.');
    } 
@@ -44,27 +44,48 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
    } 
    else if (validateInput(fuelLevel) === 'is not a Number' || validateInput(cargoLevel) === 'is not a Number'){
       alert ('The fuel level and cargo mass should be numbers.');
-     }  
-    if (validateInput(fuelLevel) < 10000){
+      //console.log(alert);
+     }  else {
+        if(fuelLevel <= 10000 && cargoLevel <= 10000){
         list.style.visibility = 'visible';
         launchStatus.innerHTML = "Shuttle not ready for launch.";   
         launchStatus.style.color = 'red';
-        fuelLevel.innerHTML = 'Shuttle does not have enough fuel to make the journey.';
+        pilotStatus.innerHTML = 
+                `Pilot ${pilot} is ready.`;
+        copilotStatus.innerHTML = `Copilot ${copilot} is ready.`;
+        fuelStatus.innerHTML = 'Shuttle does not have enough fuel to make the journey.';
+        cargoStatus.innerHTML = "Cargo mass low enough for launch.";
         } 
-    else if (validateInput(cargoLevel) > 10000){
+    else if (fuelLevel >= 10000 && cargoLevel >= 10000){
         list.style.visibility = 'visible';
         launchStatus.innerHTML = "Shuttle not ready for launch.";
         launchStatus.style.color = 'red';
+        pilotStatus.innerHTML = 
+                `Pilot ${pilot} is ready.`;
+        copilotStatus.innerHTML = `Copilot ${copilot} is ready.`;
+        fuelStatus.innerHTML = " Fuel level high enough for launch."
         cargoStatus.innerHTML = "There's too much junk in this trunk to launch.";
         }
+    else if (cargoLevel >= 10000 && fuelLevel <= 10000){
+        list.style.visibility = 'visible';
+        launchStatus.innerHTML = "Shuttle not ready for launch.";
+        launchStatus.style.color = 'red';
+        pilotStatus.innerHTML = 
+                `Pilot ${pilot} is ready.`;
+        copilotStatus.innerHTML = `Copilot ${copilot} is ready.`;
+        fuelStatus.innerHTML = "Shuttle does not have enough fuel to make the journey.";
+        cargoStatus.innerHTML = "There's too much junk in this trunk to launch.";
+    }
     else{ 
-            launchStatus.innerHTML = 'Shuttle is ready for launch.';
-            launchStatus.style.color = 'green';
-           pilotStatus.innerHTML = 
-                `Pilot ${pilot.value} is ready.`;
-            copilotStatus.innerHTML = `Copilot ${copilot.value} is ready.`;
+        list.style.visibility = 'visible';   
+        launchStatus.innerHTML = 'Shuttle is ready for launch.';
+        launchStatus.style.color = 'green';
+        pilotStatus.innerHTML = 
+                `Pilot ${pilot} is ready.`;
+        copilotStatus.innerHTML = `Copilot ${copilot} is ready.`;
          }
-        console.log(alert);
+        
+     }
     }
 
 
